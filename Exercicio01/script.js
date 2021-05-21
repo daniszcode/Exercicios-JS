3a77577313d8c0024493ea3cc37525c4e75bce69
daniszcode <danirbd13@hotmail.com>
2021-05-21 17:38:23 -0300

    Repositorio de Exercicios - JS

---
+++ Exercicio01/script.js
@@ -0,0 +1,6 @@
+let anoNascimento = 1993
+const anoAtual = 2021
+let idade =  anoAtual - anoNascimento
+let meses = idade * 12
+let dias = meses * 30
+console.log(`Sua idade é ${idade} anos. São ${meses} meses e ${dias} dias vividos até aqui.`)
