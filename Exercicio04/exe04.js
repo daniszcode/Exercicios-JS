26820e4df0c3481f5f51ea5e3d2061d6638d34f1
Daniele Marques <79206579+daniszcode@users.noreply.github.com>
2021-06-25 22:25:05 -0300

    Add files via upload

---
+++ Exercicio04/exe04.js
@@ -0,0 +1,23 @@
+
+ const alunos = [
+    { nome: "Jaja", notas: [10, 5, 10, 5] },
+    { nome: "Breno", notas: [8, 5, 2, 3] },
+    { nome: "Ana", notas: [4, 10, 9, 7] },
+    { nome: "Rodrigo", notas: [8, 10, 3, 5] },
+    { nome: "Vitor", notas: [5, 10, 10, 2] },
+  ];
+      
+  
+  function TirarnotasdoArray() {
+      
+     for (let nota of alunos){
+         let notasAlunos = nota.notas
+      console.log(notasAlunos)
+      notasAlunos.map(item => {
+          
+  
+      })
+     }
+  }
+  
+  TirarnotasdoArray()
